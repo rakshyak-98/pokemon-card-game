35a83d6fb7ac3dede03d5ddafdba3392f1282078
rakshyak-98 <rakshak1998@gmail.com>
2024-11-10 10:41:26 +0530

    make card type, interface, Boilerplate

---
+++ game-modules/cards/card-description.ts
@@ -0,0 +1,26 @@
+const DESCRIPTION = {
+	grass:
+		"Grass-type Pokemon often have attacks that heal themselves or make their opponents poisoned.",
+	fire: "Fire-type Pokemon have big attacks! They can make their opponents Burned, but their attacks need time to build up again",
+	water:
+		"Water-type Pokemon can manipulate Energy and move Pokemon around on the other team.",
+	lightning:
+		"Lightning-type Pokemon can bring back used Energy from the discard pile- and they can make their opponents Paralyzed.",
+	psychic:
+		"Psychic-type Pokemon are great for special powers! Their opponents often find themselves Asleep, Confused or Poisoned.",
+	fighting:
+		"Fighting-type Pokemon can take bigger risks to do extra damage, and some can flip coins for combination hits.",
+	darkness:
+		"Darkness-type Pokemon feature sneaky attacks that often make opponents discard cards! They can also make your opponent's Pokemon Poisoned.",
+	metal:
+		"Metal-type Pokemon can resist attacks for longer than most often Pokemon.",
+	fairy:
+		"Fairy-type Pokemon have tricks that will make opposing Pokemon's attacks less effective. Since Sword & Shild, you'll find some Pokemon that used to be Fairy types showing up as Psychic types instead.",
+	dragon:
+		"Dragon-type Pokemon have very strong attacks, but often require two Energy types to use them.",
+	colorless:
+		"Colorless-type Pokemon have lots of different moves. and they work with any kind of deck.",
+};
+
+export default DESCRIPTION;
+export type DescriptionType = typeof DESCRIPTION;
---
+++ game-modules/cards/rules.ts
@@ -0,0 +1,2 @@
+const Rules = {}
+export type RulesType = typeof Rules
